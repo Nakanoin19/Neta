98154b3ab5bdfcc0a4ed3772313a6f7a1bfe6777
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-05-10 21:09:05 +0900

    Update and rename ( ◜ᴗ◝)<ムギュルッパープエール.sh to ( ◜ᴗ◝)<ムギュルッパープエール-Zenity.sh

---
+++ ( ◜ᴗ◝)<ムギュルッパープエール-Zenity.sh
@@ -0,0 +1,11 @@
+#Zenityの練習用 ( ◜ᴗ◝)<ムギュルッパープエールが好きな人のみ使うように。
+sudo zenity --entry --title "( ◜ᴗ◝)<ムギュルッパープエール" --text "( ◜ᴗ◝)<ムギュルッパープエール" --entry-text "( ◜ᴗ◝)<ムギュルッパープエール"
+sudo zenity --entry --title "( ◜ᴗ◝)<ムギュルッパープエール" --text "( ◜ᴗ◝)<ムギュルッパープエール" --entry-text "( ◜ᴗ◝)<ムギュルッパープエール"
+sudo zenity --entry --title "( ◜ᴗ◝)<ムギュルッパープエール" --text "( ◜ᴗ◝)<ムギュルッパープエール" --entry-text "( ◜ᴗ◝)<ムギュルッパープエール"
+sudo zenity --entry --title "( ◜ᴗ◝)<ムギュルッパープエール" --text "( ◜ᴗ◝)<ムギュルッパープエール" --entry-text "( ◜ᴗ◝)<ムギュルッパープエール"
+sudo zenity --entry --title "( ◜ᴗ◝)<ムギュルッパープエール" --text "( ◜ᴗ◝)<ムギュルッパープエール" --entry-text "( ◜ᴗ◝)<ムギュルッパープエール"
+sudo zenity --entry --title "( ◜ᴗ◝)<ムギュルッパープエール" --text "( ◜ᴗ◝)<ムギュルッパープエール" --entry-text "( ◜ᴗ◝)<ムギュルッパープエール"
+sudo zenity --entry --title "( ◜ᴗ◝)<ムギュルッパープエール" --text "( ◜ᴗ◝)<ムギュルッパープエール" --entry-text "( ◜ᴗ◝)<ムギュルッパープエール"
+sudo zenity --entry --title "( ◜ᴗ◝)<ムギュルッパープエール" --text "( ◜ᴗ◝)<ムギュルッパープエール" --entry-text "( ◜ᴗ◝)<ムギュルッパープエール"
+sudo zenity --entry --title "( ◜ᴗ◝)<ムギュルッパープエール" --text "( ◜ᴗ◝)<ムギュルッパープエール" --entry-text "( ◜ᴗ◝)<ムギュルッパープエール"
+sudo zenity --entry --title "( ◜ᴗ◝)<ムギュルッパープエール" --text "( ◜ᴗ◝)<ムギュルッパープエール" --entry-text "( ◜ᴗ◝)<ムギュルッパープエール"
