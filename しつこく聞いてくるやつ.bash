b2e600deaafb9fbf313b80b117bbea8cc264af35
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-07-22 22:42:02 +0900

    Update しつこく聞いてくるやつ.bash

--- しつこく聞いてくるやつ.bash
+++ しつこく聞いてくるやつ.bash
@@ -4,8 +4,8 @@ read -n 1 -p "実行しますか?" YN
 if [[ ${YN} = [yY] ]]; then
   while true
   do
-    read -n 1 -p "本当に実行しますか?" YN
-    if [[ ${YN} -ne [yY] ]]; then
+    read -n 1 -p "本当に実行しますか?" YN1
+    if [[ ${YN1} -ne [yY] ]]; then
      exit 0;;
     else
     fi
