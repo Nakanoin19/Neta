d5fb066abde0a37e9befa68fb357f8252aa41660
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2021-03-31 14:11:45 +0900

    Update date1.rb

--- date1.rb
+++ date1.rb
@@ -1,5 +1,7 @@
 require "date"
 today = Date.today
-if #{today.mon}-#{today.mday} == 08-10
+if "#{today.mon}-#{today.mday}" == "08-10"
   puts "今日はぴねこの誕生日です。みんなでお祝いしましょう!"
+else
+  puts "今日は#{today.mon}月#{today.mday}日です。"
 end
